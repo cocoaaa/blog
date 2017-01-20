7f26c8635639e61a4bcddb8d449a51f37e07c257
Hayley Song <hjsong@mit.edu>
2017-01-20 01:11:27 -0500

    adds profile pictures and landing page

---
+++ content/about.md
@@ -0,0 +1,5 @@
+Title: Who am I?
+Date: 2016-12-22 19:00
+Status: draft
+
+This is my hello world. 
---
+++ landing_page.py
@@ -0,0 +1,48 @@
+#Landing page
+LANDING_PAGE_ABOUT = {
+	'title': '''
+		Intelligence? 
+		''',
+	'subtitle': '''
+		Semantics and Geometry for 3D perception, <br>
+		Intelligent Systems for Interaction
+		''',
+	'details': '''
+	<p>
+	Hello! 
+	My name is Hae Jin (Hayley), and I'm currently a Master student at <a href="#">MIT EECS</a>
+	(concentration: AI) under the joint	supervision of Professor. <a href="https://people.csail.mit.edu/regina/">Regina Barzilay</a>, 
+	Professor. <a href="http://people.csail.mit.edu/wojciech/">Wojciech Matusik</a>, and
+	a Ph.D candidate, <a href="http://people.csail.mit.edu/jstraub/">Julian Straub</a>.
+	
+	Before my Masters, I studied Mathematics and EECS at MIT for my undergraduate studies.
+	</p>
+
+	<p>
+	I'm curious about what we call 'intelligence', especially the three-dimensional perception.
+	<ul>
+		<li>Recognition: What makes us recognize an object as what it is 
+			(e.g. a bird as a bird, a bull as a bull)? 
+			What is	the necessary and sufficient representation of an object for human recognition?  
+			This question has been lingering on my mind ever since I saw a video of Picasso at work:
+		</li>
+
+			<iframe width="560" height="315" 
+				src="https://www.youtube.com/embed/ow7eEWh37iU?autoplay=0" 
+				frameborder="0" allowfullscreen>
+			</iframe>
+
+		<li>Understanding of the functional and causal relations between 
+			objects in a visual scene
+		</li>
+
+		<li>Holistic scene interpretation by combining the semantical and geometric
+		information from 2D images and 3D data (such as RGB-D)
+		</li>
+	</ul>
+	</p>
+	''',
+	'img_path': '''
+		./content/images/profile.jpg
+	'''
+}
