e7ee11758c573d904285b4a3f989e1c39356a8ce
Hayley Song <hjsong@mit.edu>
2017-01-24 13:32:49 -0500

    adds theme with the tree structure as in talha131.github.io tree 15a09b5cd5'

--- projects.py
+++ projects.py
@@ -1,9 +1,47 @@
 PROJECTS = [
-	{
-	
-	},
-
-	{},
-	
-
-]
+        {
+            'name': 'Elegant',
+            'url':
+            'http://oncrashreboot.com/pelican-elegant',
+            'description': 'A clean and distraction free Pelican theme, with'
+            ' search and a'
+            ' lot more unique features. Built with Jinja2 and Bootstrap'},
+        {
+            'name': 'Logpad + Duration',
+            'url':
+            'https://github.com/talha131/logpad-plus-duration#logpad--duration',
+            'description': 'Vim plugin to emulate Windows Notepad logging feature,'
+            ' and log duration of each entry'},
+        {
+            'name': 'Pelican',
+            'url':
+            'https://github.com/getpelican/pelican/commits?author=talha131',
+            'description': 'Static site generator that supports Markdown and'
+            ' reST syntax'},
+        {
+            'name': 'Pelican Plugins',
+            'url':
+            'https://github.com/getpelican/pelican-plugins/commits?author=talha131',
+            'description': 'Bunch of plugins for Pelican blog engine'},
+        {
+            'name': 'Asana to Github Issues',
+            'url': 'https://github.com/talha131/AsanaToGithub#asana-to-github',
+            'description': 'Command-line program to move your tasks from Asana'
+            ' projects to Github issues'},
+        {
+            'name': 'Asana',
+            'url':
+            'https://github.com/pandemicsyn/asana/commits?author=talha131',
+            'description': 'Python Asana API bindings'},
+        {
+            'name': 'Ctags',
+            'url':
+            'https://github.com/fishman/ctags/commits?author=talha131',
+            'description': 'Exuberant Ctags clone with ObjectiveC and CSS support'},
+        {
+            'name': 'Wasavi',
+            'url':
+            'https://github.com/akahuku/wasavi/commits?author=talha131',
+            'description': 'A browser extension that changes textarea element to'
+            ' Vi editor'
+            }]
